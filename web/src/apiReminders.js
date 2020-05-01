0e4c265dfea74e3b6775cec4ccdd582c38266fc3
Glitch (ablaze-destiny-dish) <none>
2020-05-01 14:50:14 +0000

    Fix: hosting with glitch

--- web/src/apiReminders.js
+++ web/src/apiReminders.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require('https');
-const helper = require("./helper.js");
+const helper = require("./botHelper.js");
 
 const app = express();
 app.use(bodyParser.json());
