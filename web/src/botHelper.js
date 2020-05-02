19dcb046e7a307876fab8150b13ec0b44e90ed01
gadnlino <guiavenas@gmail.com>
2020-05-02 19:56:56 -0300

    fix

--- web/src/botHelper.js
+++ web/src/botHelper.js
@@ -6,24 +6,6 @@ const awsSvc = require("./services/awsService.js");
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 const bot = new Telegraf(TELEGRAM_TOKEN);
 
-bot.on('callback_query', async ctx => {
-    const uuid = ctx.update.callback_query.data;
-
-    console.log("deleting reminder...")
-
-    const response = await awsSvc.dynamodb.updateItem(remindersTableName, {
-        "uuid": uuid
-    },
-        "set dismissed= :d",
-        {
-            ":d": true
-        });
-
-    if (response.$response.httpResponse.statusCode === 200) {
-        ctx.reply("Lembrete apagado");
-    }
-});
-
 module.exports = {
 
     sendReminder: async (reminder) => {
@@ -31,7 +13,7 @@ module.exports = {
         bot.telegram
             .sendMessage(reminder.chat_id, `Lembrete : ${reminder.body}`,
                 Extra.markup(Markup.inlineKeyboard([
-                    Markup.callbackButton('Já lembrei!', reminder.uuid)
+                    Markup.callbackButton('Já lembrei!', reminder.uuid, true)
                 ])));
     }
 }
