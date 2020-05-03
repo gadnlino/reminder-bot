03c17cc1175ff5f848dd4a9fceb0d6fd48983536
Glitch (remindja-bot) <none>
2020-05-03 01:09:41 +0000

    Adicionando preview das novas funcionalidades

---
+++ src/reminderPoller.js
@@ -0,0 +1,32 @@
+const awsSvc = require("./services/awsService.js");
+const botHelper = require("./botHelper.js");
+
+module.exports = () => {
+  
+  const {REMINDERS_QUEUE_URL, POLL_INTERVAL} = process.env;
+
+  async function pollSqsQueueAndSendReminders() {
+    
+    console.log("polling reminders...");
+    
+    const getMessagesResp = await awsSvc.sqs
+              .getMessages(REMINDERS_QUEUE_URL);
+
+    const messages = getMessagesResp.Messages;
+
+    console.log(messages);
+
+    if(messages && messages.length > 0){
+
+        messages.forEach(async message=>{
+
+            const reminder = JSON.parse(message.Body);
+            console.log("Sending reminder: " + reminder.body);
+            await botHelper.sendReminder(reminder);
+            await awsSvc.sqs.deleteMessage(REMINDERS_QUEUE_URL, message.ReceiptHandle);
+        });        
+    }
+  }
+  
+  setInterval(pollSqsQueueAndSendReminders, parseInt(POLL_INTERVAL));
+}
